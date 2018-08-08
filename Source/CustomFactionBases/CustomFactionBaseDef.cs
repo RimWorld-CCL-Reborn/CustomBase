25ac52ba13b8fc4eaf5e00e4e011a9314aeeae56
erdelf <erdelf.productions@gmail.com>
2018-08-08 17:45:26 +0200

    fixed z flipping

--- Source/CustomFactionBases/CustomFactionBaseDef.cs
+++ Source/CustomFactionBases/CustomFactionBaseDef.cs
@@ -16,7 +16,7 @@
         public List<Substitute> substitutes = new List<Substitute>();
         public List<SubstituteExtended> extendedSubstitutes = new List<SubstituteExtended>();
 
-        public string baseBuildings;
+        public List<string> baseBuildings;
 
 
         public override void ResolveReferences()
@@ -32,7 +32,7 @@
 
         public CellRect GetRect(ResolveParams rp)
         {
-            string[] buildingLines = this.baseBuildings.Split('\n');
+            string[] buildingLines = this.baseBuildings[index: 0].Split('\n');
             int width = buildingLines.Max(selector: s => s.Length);
 
             return new CellRect(minX: rp.rect.CenterCell.x - width / 2, minZ: rp.rect.CenterCell.z - buildingLines.Length / 2,
@@ -41,11 +41,11 @@
 
         public void GenerateBase(ResolveParams rp)
         {
-            string[] buildingLines = this.baseBuildings.Split('\n');
-            
+            string[] buildingLines = this.baseBuildings.RandomElement().Split('\n').Reverse().ToArray();
+
             for (int z = rp.rect.minZ; z <= rp.rect.maxZ; z++)
             {
-                for (int x = rp.rect.minX; x <= rp.rect.maxX && x - rp.rect.minX < buildingLines[z-rp.rect.minZ].Length; x++)
+                for (int x = rp.rect.minX; x <= rp.rect.maxX && x - rp.rect.minX < buildingLines[z - rp.rect.minZ].Length; x++)
                 {
                     if (this.substituteDictionary.TryGetValue(key: buildingLines[z - rp.rect.minZ][index: x - rp.rect.minX], value: out SubstituteBase sub))
                         GenSpawn.Spawn(newThing: sub.GetBuilding(), loc: new IntVec3(newX: x, newY: 0, newZ: z), map: BaseGen.globalSettings.map);
